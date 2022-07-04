e6ea6a690ef6826129270298609b5d3cfd552a83
AKHTAR ALI <56265099+AKHTARALI879@users.noreply.github.com>
2022-07-05 02:54:38 +0300

    Add files via upload

---
+++ spell_checker.py
@@ -0,0 +1,12 @@
+from textblob import TextBlob
+
+word = input("write word here: ")
+
+word_correct = TextBlob(word)
+
+print(word_correct.correct())
+
+#That's it for today
+#I hope you like this small project
+#see you in my next video
+#TAKE CARE
